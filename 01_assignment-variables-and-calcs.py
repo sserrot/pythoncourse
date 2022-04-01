71b208d2a725dfe633c9382337ae258568ac5fce
Santiago Torres <sserrot@users.noreply.github.com>
2022-04-01 09:31:32 -0400

    assignment 1

--- 01_assignment-variables-and-calcs.py
+++ 01_assignment-variables-and-calcs.py
@@ -3,7 +3,7 @@ Assignment #1
 
 1. Add / modify code ONLY between the marked areas (i.e. "Place code below")
 2. Run the associated test harness for a basic check on completeness. A successful run of the test cases does not guarantee accuracy or fulfillment of the requirements. Please do not submit your work if test cases fail.
-3. To run unit tests simply use the below command after filling in all of the code:
+3. To run unit tests simply use the below command after filling in all the code:
     python 01_assignment.py
   
 4. Unless explicitly stated, please do not import any additional libraries but feel free to use built-in Python packages
@@ -32,7 +32,7 @@ def exercise01():
     # Create a variable x and set it to 5.
 
     # ------ Place code below here \/ \/ \/ ------
-
+    x = 5
     # ------ Place code above here /\ /\ /\ ------
 
     return x
@@ -42,7 +42,7 @@ def exercise02():
     # Create a string variable called name and set it to your first name.
 
     # ------ Place code below here \/ \/ \/ ------
-
+    name = "Santiago"
     # ------ Place code above here /\ /\ /\ ------
 
     return name
@@ -52,7 +52,7 @@ def exercise03():
     # Create a string variable called sentence and assign it to an arbitrary sentence that contains at least 3 words
 
     # ------ Place code below here \/ \/ \/ ------
-
+    sentence = "Fear is Evil"
     # ------ Place code above here /\ /\ /\ ------
 
     return sentence
@@ -62,7 +62,8 @@ def exercise04():
     # Create two string variables. The first variable is called first_name, the second is last_name. Set both variables to your first name and last name respectively.
 
     # ------ Place code below here \/ \/ \/ ------
-
+    first_name = "Santiago"
+    last_name = "Torres"
     # ------ Place code above here /\ /\ /\ ------
 
     return first_name, last_name
@@ -73,7 +74,9 @@ def exercise05():
     # Repeate exercise 4 here and assign the datatype of the variable first_name to a variable called name_type
 
     # ------ Place code below here \/ \/ \/ ------
-
+    first_name = "Santiago"
+    last_name = "Torres"
+    name_type = type(first_name)
     # ------ Place code above here /\ /\ /\ ------
 
     return first_name, last_name, name_type
@@ -83,7 +86,9 @@ def exercise06():
     # Assign 20 to the variable hours_worked, 15 to the variable wage_per_hour and the product of the two to variable total_pay
 
     # ------ Place code below here \/ \/ \/ ------
-
+    hours_worked = 20
+    wage_per_hour = 15
+    total_pay = hours_worked * wage_per_hour
     # ------ Place code above here /\ /\ /\ ------
     return hours_worked, wage_per_hour, total_pay
 
@@ -91,7 +96,9 @@ def exercise06():
 def exercise07():
     # Create a variable wage and assign 17.0 to it. Print to the screen the datatype of wage. Create a second variable called doubled that prints to the screen 2 times wage
     # ------ Place code below here \/ \/ \/ ------
-
+    wage = 17.0
+    print(type(wage))
+    doubled = 2*wage
     # ------ Place code above here /\ /\ /\ ------
     return wage, doubled
 
@@ -100,7 +107,9 @@ def exercise08():
     # Assign 5 to the variable quantity, 'hello' to the variable hello and a variable hello_repeated that holds a string that contains whatever is contained in the variable hello repeated quantity times
 
     # ------ Place code below here \/ \/ \/ ------
-
+    quantity = 5
+    hello = 'hello'
+    hello_repeated = hello * quantity
     # ------ Place code above here /\ /\ /\ ------
 
     return quantity, hello, hello_repeated
@@ -110,7 +119,9 @@ def exercise09():
     # Assign 10 to a variable qty, 5 to a variable price and the product of the two to a variable total_cost
 
     # ------ Place code below here \/ \/ \/ ------
-
+    qty = 10
+    price = 5
+    total_cost = qty * price
     # ------ Place code above here /\ /\ /\ ------
 
     return qty, price, total_cost
@@ -120,7 +131,12 @@ def exercise10():
     # Create 5 variables named factorN where N is the numbers 1 to 5 and set them to 1 through 5, respectively. Create a variable called product that holds the product of the 5 variables
 
     # ------ Place code below here \/ \/ \/ ------
-
+    factor1 = 1
+    factor2 = 2
+    factor3 = 3
+    factor4 = 4
+    factor5 = 5
+    product = factor1 * factor5 * factor4 * factor3 * factor2
     # ------ Place code above here /\ /\ /\ ------
 
     return factor1, factor2, factor3, factor4, factor5, product
@@ -130,7 +146,7 @@ def exercise11():
     # Create a variable pi and literally set it to pi 10 decimal places out
 
     # ------ Place code below here \/ \/ \/ ------
-
+    pi = format(math.pi, ".11")
     # ------ Place code above here /\ /\ /\ ------
     return pi
 
@@ -139,7 +155,8 @@ def exercise12():
     # Create a variable called x and set it to 10. Then create a variable y that equals to x to the 7th power
 
     # ------ Place code below here \/ \/ \/ ------
-
+    x = 10
+    y = x**7
     # ------ Place code above here /\ /\ /\ ------
     return x, y
 
@@ -149,7 +166,8 @@ def exercise13():
 
     pi = 3.14159
     # ------ Place code below here \/ \/ \/ ------
-
+    r = 7
+    volume_sphere = 4/3 * pi * r**3
     # ------ Place code above here /\ /\ /\ ------
     return pi, r, volume_sphere
 
@@ -158,7 +176,10 @@ def exercise14():
     # Create a variables area, length, height. Set length and height equal to 50 and 10.2 respectively. Assign area to the product of length and height, assign the variable area_type to the datatype of area
 
     # ------ Place code below here \/ \/ \/ ------
-
+    length = 50
+    height = 10.2
+    area = length * height
+    area_type = type(area)
     # ------ Place code above here /\ /\ /\ ------
     return area, length, height, area_type
 
@@ -167,7 +188,9 @@ def exercise15():
     # Calculate the distance covered by a car moving at 80 miles per hour for 3 hours
 
     # ------ Place code below here \/ \/ \/ ------
-
+    duration = 3
+    speed_mph = 80
+    distance = duration * speed_mph
     # ------ Place code above here /\ /\ /\ ------
 
     return distance, speed_mph, duration
@@ -177,7 +200,9 @@ def exercise16():
     # Implement pythogorean thereom and find the length of hypotenuse c given sides a and b. Select any numbers for a and b. Use math.sqrt() for square root.
 
     # ------ Place code below here \/ \/ \/ ------
-
+    a = 5
+    b = 3
+    c = math.sqrt(a**2 + b**2)
     # ------ Place code above here /\ /\ /\ ------
     return a, b, c
 
